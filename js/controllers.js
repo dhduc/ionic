f18f67b6309a80ba1593403741061be64408a4a5
Duc Dao Huu <huuduc.uneti@gmail.com>
2016-06-01 18:23:22 +0700

    Update API

--- js/controllers.js
+++ js/controllers.js
@@ -4,8 +4,16 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('blogCtrl', function($scope) {
-
+.controller('blogCtrl', function($scope, $http) {
+    //$scope.items = BlankFactory.all();
+    console.log('controller');
+    $http.get("http://project.wordpress.local/wp-json/wp/v2/posts/1")
+            .success(function(data) {
+                console.log(data);
+                $scope.post = data;
+            })
+            .error(function(data) {
+            });
 })
    
 .controller('aboutPageCtrl', function($scope) {
